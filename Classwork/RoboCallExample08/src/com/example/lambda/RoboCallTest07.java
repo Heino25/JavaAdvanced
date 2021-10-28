4cd80cc20533ae81a3502eed99928460c1069ee8
Heino <heinoduplessis@gmail.com>
2021-10-28 08:52:34 +0200

    28/10/21

---
+++ Classwork/RoboCallExample08/src/com/example/lambda/RoboCallTest07.java
@@ -0,0 +1,32 @@
+package com.example.lambda;
+
+import java.util.List;
+import java.util.function.Predicate;
+
+/**
+ *
+ * @author MikeW
+ */
+public class RoboCallTest07 {
+  
+  public static void main(String[] args){ 
+
+    List<Person> pl = Person.createShortList();
+    RoboCall05 robo = new RoboCall05();    
+    
+    System.out.println("\n=== Calling all Drivers Lambda ===");
+    pl.stream()
+        .filter(p -> p.getAge() >= 23 && p.getAge() <= 65)
+        .forEach(p -> robo.roboCall(p));
+    
+    System.out.println("\n=== Calling all Draftees Lambda ===");
+    pl.stream()
+            .filter(p -> p.getAge() >= 18 && p.getAge() <=25 && p.getGender() == Gender.MALE)
+            .forEach(p -> robo.roboCall(p));
+            
+    System.out.println("\n=== Calling all Pilots Lambda ===");
+    pl.stream()
+            .filter(p -> p.getAge() >= 23 && p.getAge() <=65)
+            .forEach(p -> robo.roboCall(p));        
+  }
+}
