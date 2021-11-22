9be3508d0099b3474967c332897d8af20dfb20c0
Heino <heinoduplessis@gmail.com>
2021-11-22 09:43:03 +0200

    22/11/21

--- JavaMasterClassVid+13/ToDoList/src/main/java/com/example/todolist/Main.java
+++ JavaMasterClassVid+13/ToDoList/src/main/java/com/example/todolist/Main.java
@@ -1,18 +1,19 @@
 package com.example.todolist;
 
+import com.example.todolist.datamodel.TodoData;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.util.Objects;
+import java.io.IOException;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("mainwindow.fxml")));
+        Parent root = FXMLLoader.load(getClass().getResource("mainwindow.fxml"));
         primaryStage.setTitle("Todo List");
         primaryStage.setScene(new Scene(root, 900, 500));
         primaryStage.show();
@@ -21,5 +22,25 @@ public class Main extends Application {
 
     public static void main(String[] args) {
         launch(args);
+    }
+
+    @Override
+    public void stop() throws Exception {
+        try {
+            TodoData.getInstance().storeTodoItems();
+
+        } catch(IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    @Override
+    public void init() throws Exception {
+        try {
+            TodoData.getInstance().loadTodoItems();
+
+        } catch(IOException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
